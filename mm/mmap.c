26cd2981434e35241ef1a297f075e513976f1cb4
DogEZ <ddogezd@gmail.com>
2025-10-12 07:11:47 +0800

    mm: remove user/admin reserve kbytes initialization for Android
    Android does not rely on sysctl_user_reserve_kbytes or
    sysctl_admin_reserve_kbytes, so the initialization
    based on free memory is unnecessary.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -3784,11 +3784,6 @@ void __init mmap_init(void)
  */
 static int init_user_reserve(void)
 {
-	unsigned long free_kbytes;
-
-	free_kbytes = global_zone_page_state(NR_FREE_PAGES) << (PAGE_SHIFT - 10);
-
-	sysctl_user_reserve_kbytes = min(free_kbytes / 32, 1UL << 17);
 	return 0;
 }
 subsys_initcall(init_user_reserve);
@@ -3805,11 +3800,6 @@ subsys_initcall(init_user_reserve);
  */
 static int init_admin_reserve(void)
 {
-	unsigned long free_kbytes;
-
-	free_kbytes = global_zone_page_state(NR_FREE_PAGES) << (PAGE_SHIFT - 10);
-
-	sysctl_admin_reserve_kbytes = min(free_kbytes / 32, 1UL << 13);
 	return 0;
 }
 subsys_initcall(init_admin_reserve);
